02c5dec90272d6daa7a5a55ff573b44ee7a772b1
Laurids Andersen <lauridsandersen2013@gmail.com>
2022-05-10 13:03:57 +0200

    Laurids Hjaelper

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'elements/' + $(this).data('include') + '.html'
-      $(this).load(file)
-    })
-  })
+// $(function () {
+//     var includes = $('[data-include]')
+//     $.each(includes, function () {
+//       var file = 'elements/' + $(this).data('include') + '.html'
+//       $(this).load(file)
+//     })
+//   })

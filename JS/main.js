a3a179c69eb5a851aa4d40abf4da85bc4ab20ab4
Michael <hovgaard1990@gmail.com>
2022-05-11 10:04:00 +0200

    flytnig

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,8 @@
 // https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
 
 
-// elements
+// Vi bruger jq for at til at indhente html filer inde i vores html dokument, vi gør dette for at kunne arbejde på flere elementer til siderne og på den måde kunne arbejde langt mere effektivt på siden. 
+ 
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
